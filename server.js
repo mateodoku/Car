6d9444448aaa894ad2de4f114c55a1d5fd9473b6
mateodoku <mateodoku8@gmail.com>
2026-07-15 18:25:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http=require('http');
 const fs=require('fs');
 const path=require('path');
-const root=path.join(__dirname,'public');
+const root = __dirname;
 const types={'.html':'text/html; charset=utf-8','.css':'text/css; charset=utf-8','.js':'application/javascript; charset=utf-8','.png':'image/png','.jpg':'image/jpeg','.jpeg':'image/jpeg','.svg':'image/svg+xml'};
 http.createServer((req,res)=>{
   let p=decodeURIComponent(req.url.split('?')[0]);
