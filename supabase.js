d5d10a4b36284b6f5d855c7e9e06c2d7afee79fd
mateodoku <mateodoku8@gmail.com>
2026-07-15 21:21:31 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,4 +2,3 @@ const supabaseUrl = "https://gcqqhwsjqphaygjxtqdy.supabase.co";
 const supabaseKey = "sb_publishable_9wA0UzltX30hwMdhZynvmg_p_zy8oHb";
 
 const db = supabase.createClient(supabaseUrl, supabaseKey);
-const supabaseKey = "sb_publishable_...";
